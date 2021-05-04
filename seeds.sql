cfdf7431cd18d6b11a05fa1eeb18a3772d8526f1
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-03 21:01:55 -0500

    added what I think to be the seeds

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Junior Developer", 65000,);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Levi", "Kane");
