4aa3486c8ad8c88f1a5145d8b79a86a9507440c3
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-03 20:55:50 -0500

    installed packages and built out schema

---
+++ index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+var inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  password: 'password',
+  database: 'business_db'
+});
