d2448212aa2ebd6eb672b0d3c79bd27e9bcf9445
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-04 08:57:44 -0500

    have initial functions set up with switch statement

--- index.js
+++ index.js
@@ -14,3 +14,81 @@ const connection = mysql.createConnection({
   password: 'password',
   database: 'business_db'
 });
+
+function viewAllEmployees() {}
+
+function viewAllRoles() {}
+
+function viewEmployeesByDepartment() {}
+
+function viewEmployeesByManager() {}
+
+function addEmployee() {}
+
+function removeEmployee() {}
+
+function updateEmployeeRole() {}
+
+function updateEmployeeManager() {}
+
+function init() {
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+        name: 'initialSelection',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all Employees',
+          'View all Roles',
+          'View employees by Department',
+          'View employees by Manager',
+          'Add Employee',
+          'Remove Employee',
+          'Update Employee Role',
+          'Update Employee Manager'
+        ]
+      }
+    ])
+    .then((answer) => {
+      // Use user feedback for... whatever!!
+      console.log(answer.initialSearch);
+      //   Use user feedback for... whatever!!
+      switch (answer.initialSearch) {
+        case 'View all Employees':
+          viewAllEmployees();
+          break;
+        case 'View all Roles':
+          viewAllRoles();
+          break;
+        case 'View employees by Department':
+          viewEmployeesByDepartment();
+          break;
+        case 'View employees by Manager':
+          viewEmployeesByManager();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Remove Employee':
+          removeEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'Update Employee Manager':
+          updateEmployeeManager();
+          break;
+        default:
+          connection.end();
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
